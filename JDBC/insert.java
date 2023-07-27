8b5d8ca81cebf1600f6d8b6ce353b9aba750949d
Tejas Dharmik <84400584+TejasDharmik12@users.noreply.github.com>
2023-07-27 19:51:36 +0530

    Create insert.java

---
+++ JDBC/insert.java
@@ -0,0 +1,42 @@
+package jdbcInsertDemo;
+//import java.lang.NoClassDefFoundError;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+public class insert {
+	public static void main(String []args) throws Exception {
+		
+		String name = "ABC";
+		String email = "abc@gmail.com";
+		String password = "ab";
+		String gender = "Male";
+		String city = "Chandigarh";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_db","root","tejas@mysql123");
+		System.out.println("Driver class loaded successfully");
+		
+		
+//		Three ways to add in the database
+//		one
+//		PreparedStatement ps = con.prepareStatement("insert into register values('tejas','tejas@gmail.com','deepak123','male','nagpur')");
+//		two
+//		PreparedStatement ps = con.prepareStatement("insert into register values('"+name+"','"+email+"','"+password+"','"+gender+"','"+city+"')");
+//		three
+		PreparedStatement ps = con.prepareStatement("insert into register values(?,?,?,?,?)");
+		ps.setString(1, name);
+		ps.setString(2, email);
+		ps.setString(3, password);
+		ps.setString(4, gender);
+		ps.setString(5, city);
+		int i = ps.executeUpdate();
+		if(i>0) {
+			System.out.println("success");
+		}
+		else {
+			System.out.println("fail");
+		}
+		ps.close();
+		con.close();
+	}
+}
