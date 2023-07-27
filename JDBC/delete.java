8b7b44c7eb475cbdb12e2cc8dd82450759fa5d71
Tejas Dharmik <84400584+TejasDharmik12@users.noreply.github.com>
2023-07-27 23:07:48 +0530

    Create delete.java

---
+++ JDBC/delete.java
@@ -0,0 +1,45 @@
+package jdbcInsertDemo;
+//import java.lang.NoClassDefFoundError;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+public class insert {
+	public static void main(String []args) throws Exception {
+		
+//		String name = "ABC";
+		String email = "abc@gmail.com";
+//		String password = "ab";
+//		String gender = "Male";
+//		String city = "Yavatmad";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/jdbc_db","root","tejas@mysql123");
+		System.out.println("Driver class loaded successfully");
+		
+		
+//		Three ways to add in the database
+//		one
+//		PreparedStatement ps = con.prepareStatement("insert into register values('tejas','tejas@gmail.com','deepak123','male','nagpur')");
+//		two
+//		PreparedStatement ps = con.prepareStatement("insert into register values('"+name+"','"+email+"','"+password+"','"+gender+"','"+city+"')");
+//		three
+//		PreparedStatement ps = con.prepareStatement("insert into register values(?,?,?,?,?)");
+
+//		PreparedStatement ps = con.prepareStatement("update register set city=? where email=?");
+		PreparedStatement ps = con.prepareStatement("delete from register where email=?");
+//		ps.setString(1, name);
+		ps.setString(1, email);
+//		ps.setString(3, password);
+//		ps.setString(4, gender);
+//		ps.setString(2, email);
+		int i = ps.executeUpdate();
+		if(i>0) {
+			System.out.println("success");
+		}
+		else {
+			System.out.println("fail");
+		}
+		ps.close();
+		con.close();
+	}
+}
